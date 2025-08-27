dfe41718b5b3bee8d67e36009a5a211642642f9c
SouzaMatheus11 <matheus@cpninformatica.com.br>
2025-08-27 10:03:59 -0300

    Separando arquivos

--- controllers/climaController.js
+++ controllers/climaController.js
@@ -1,1 +1,35 @@
- 
+const climaService = require('../services/climaService');
+
+async function getClima(req, res) {
+    console.log('Requisição recebida na rota /clima. Parâmetros:', req.query);
+
+    const { cidade } = req.query;
+
+    if (!cidade) {
+        return res.status(400).json({ error: 'Cidade é obrigatória' });
+    }
+
+    try {
+        const data = await climaService.fetchClima(cidade);
+
+
+        const formattedData = {
+            cidade: data.name,
+            pais: data.sys.country,
+            temperatura: data.main.temp + "°C",
+            sensacao_termica: data.main.feels_like + "°C",
+            condicao: data.weather[0].description,
+            umidade: data.main.humidity + "%",
+            velocidade_vento: data.wind.speed,
+            icone: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+        };
+
+        res.json(formattedData);
+    } catch (error) {
+        if (error.response && error.response.status === 404)
+            return res.status(400).json({ error: "cidade não encontrada." });
+        res.status(500).json({ error: "Erro ao buscar clima." });
+    }
+}
+
+module.exports = { getClima };
--- index.js
+++ index.js
@@ -1,42 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
+const climaRoutes = require('./routes/climaRoutes');
 
 const app = express();
 const port = 3000;
 
-const API_KEY = process.env.API_KEY;
-const API_URL = process.env.BASE_URL;
-
-app.get('/clima', async(req, res) => {
-    const cidade = req.query.cidade;
-
-    if(!cidade){
-        return res.status(400).json({error: 'Cidade é obrigatória'});
-    }
-
-    try{
-        const response = await axios.get(`${API_URL}?q=${cidade}&APPID=${API_KEY}&units=metric&lang=pt_br`);
-        const data = response.data;
-
-        const formattedData ={
-            cidade: data.name,
-            pais: data.sys.country,
-            temperatura: data.main.temp + "°C",
-            sensacao_termica: data.main.feels_like + "°C",
-            condicao: data.weather[0].description,
-            umidade: data.main.humidity + "%",
-            velocidade_vento: data.wind.speed,
-            icone: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-        }
-
-        res.json(formattedData);
-    }catch(error){
-        if(error.response && error.response.status === 404)
-            return res.status(400).json({error: "cidade não encontrada."});
-        res.status(500).json({error: "Erro ao buscar clima."});
-    }
+// Middleware de log de requisições
+app.use((req, res, next) => {
+    const timestamp = new Date().toISOString();
+    console.log(`[${timestamp}] ${req.method} ${req.originalUrl}`);
+    next(); // Passa a requisição para o próximo middleware ou rota
 });
+
+app.use('/clima', climaRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
